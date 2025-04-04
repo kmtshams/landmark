3dbd02d07656b138f2f1c9086dc7f92b05bf68f0
kmtshams <9246096+kmtshams@users.noreply.github.com>
2025-04-04 16:43:00 -0700

    images fix 3

--- app/properties/[property_id]/page.jsx
+++ app/properties/[property_id]/page.jsx
@@ -3,34 +3,31 @@ import properties from '../../../public/properties/properties.json';
 import fs from 'fs';
 import path from 'path';
 
-// console.log("properties:", properties)
-
 const PropertyPage = async ({ params }) => {
     const { property_id } = await params;
     const property = properties.at(Number(property_id));
     return (
         <main>
-            <div className="min-h-screen flex items-center justify-start p-8 bg-red-100 bg-cover bg-center relative" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),  url(/properties/${property.id}/showcase.jpeg)` }}>
+            <div className="min-h-screen flex items-center justify-start p-8 bg-red-100 bg-cover bg-center relative" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/properties/${property.id}/showcase.jpeg)` }}>
                 <div className="max-w-2xl text-white bg-black/30 backdrop-blur-sm w-1/2 p-10">
-                <p className="text-left text-2xl">{property?.location}</p>
-                <h1 className="mb-4 text-6xl font-bold text-left">{property?.name}</h1>
-                <p className="text-left text-xl">{ property?.guests} Guests |  {property?.beds} Beds | {property?.baths} Baths | {property?.sqft} sqft</p>
+                    <p className="text-left text-2xl">{property?.location}</p>
+                    <h1 className="mb-4 text-6xl font-bold text-left">{property?.name}</h1>
+                    <p className="text-left text-xl">{property?.guests} Guests | {property?.beds} Beds | {property?.baths} Baths | {property?.sqft} sqft</p>
                 </div>
-            </div >
+            </div>
             <div className="flex flex-wrap min-h-screen p-8 pt-30 bg-cover bg-center relative bg-black">
-                <div className="w-1/2">                
-                    <h1 className='text-white text-2xl font-bold mb-5'>Description</h1>
-                    <pre className="description whitespace-pre-wrap break-words">{property.description}</pre>
-                </div>
-                <div className="w-1/2 ">                
-                    <div className="image-gallery flex flex-wrap justify-center">
+                <div className="grid grid-cols-2 gap-5">
+                    <div className="col-span-1">
+                        <h1 className='text-white text-2xl font-bold mb-5'>Description</h1>
+                        <pre className="description whitespace-pre-wrap break-words">{property.description}</pre>
+                    </div>
+                    <div className="col-span-1 grid grid-cols-2 gap-5">
                         {Array.from({ length: property.numPics }, (_, index) => (
-                            <img key={index} src={`/properties/${property.id}/pics/${index}.jpeg`} alt={`Property Image ${index + 1}`} className="property-image w-75 h-75 m-5" />
+                            <img key={index} src={`/properties/${property.id}/pics/${index}.jpeg`} alt={`Property Image ${index + 1}`} className="property-image w-full h-75 object-cover" />
                         ))}
                     </div>
-                </div>  
+                </div>
             </div>
-           
         </main>
     );
 };
