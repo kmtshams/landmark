3f946273393a7d2f81df13dfee2b5183a6a2e883
kmtshams <9246096+kmtshams@users.noreply.github.com>
2025-04-04 16:03:43 -0700

    images file path try fix 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    output: "standalone",
+    experimental: {
+      turbotrace: {}, 
+    },
+  };
 
 export default nextConfig;
